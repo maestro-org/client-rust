307516b5d5be869ee8119498282140c246f5bff5
jmhrpr <25673452+jmhrpr@users.noreply.github.com>
2025-10-23 17:37:47 +0100

    reduce logging

--- src/pd/client.rs
+++ src/pd/client.rs
@@ -6,7 +6,7 @@ use std::sync::Arc;
 use async_trait::async_trait;
 use futures::prelude::*;
 use futures::stream::BoxStream;
-use log::info;
+use log::debug;
 use tokio::sync::RwLock;
 
 use crate::compat::stream_fn;
@@ -329,7 +329,7 @@ impl<KvC: KvConnect + Send + Sync + 'static, Cl> PdRpcClient<KvC, Cl> {
         if let Some(client) = self.kv_client_cache.read().await.get(address) {
             return Ok(client.clone());
         };
-        info!("connect to tikv endpoint: {:?}", address);
+        debug!("connect to tikv endpoint: {:?}", address);
         match self.kv_connect.connect(address).await {
             Ok(client) => {
                 self.kv_client_cache
